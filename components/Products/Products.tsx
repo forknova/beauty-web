8c79206b3645dceb9729fa62fb9de0e13c8daca4
forknova <softechstar.jsh@outlook.com>
2023-09-20 20:00:04 +0100

    fix: fetching data directly in RSC instead of using Route Handler as per nextjs docs.

--- components/Products/Products.tsx
+++ components/Products/Products.tsx
@@ -1,15 +1,66 @@
 import Image from 'next/image';
 
+const endpoint = process.env.SHOPIFY_STOREFRONT_API_ENDPOINT as string;
+
+const query = `
+    query {
+      products(first: 10) {
+        edges {
+          node {
+            id
+            title
+            description
+            images(first: 5) {
+              edges {
+                node {
+                  originalSrc
+                  altText
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+const options = {
+  method: 'POST',
+  headers: {
+    'X-Shopify-Storefront-Access-Token': process.env
+      .SHOPIFY_STOREFRONT_ACCESS_TOKEN as string,
+    'Content-Type': 'application/json',
+  },
+};
+
 const Products = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/shopify/products`,
-  );
+  const res = await fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      'X-Shopify-Storefront-Access-Token': process.env
+        .SHOPIFY_STOREFRONT_ACCESS_TOKEN as string,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+    }),
+  });
 
   const products = await res.json();
 
+  const parsedProducts = products.data.products.edges.map((edge) => ({
+    id: edge.node.id,
+    title: edge.node.title,
+    description: edge.node.description,
+    image:
+      edge.node.images.edges.length > 0
+        ? edge.node.images.edges[0].node.originalSrc
+        : null,
+  }));
+
   return (
     <section>
-      {products.map(
+      {parsedProducts.map(
         (product: {
           id: string;
           title: string;
