8d8cd878f1fc8371be89b0e5c551b897ae9ba98b
forknova <softechstar.jsh@outlook.com>
2023-12-29 16:59:03 +0000

    chore: removed deprecated domains in favour of remotePatterns. Using turbo flag for dev.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-    domains: ["cdn.shopify.com"]
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.shopify.com",
+        pathname: "**",
+      }
+    ]
   },
 }
 
